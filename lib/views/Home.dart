7283d110b9ca915e102323d8287f9e4070bf6f57
sunell <hs0647@gmail.com>
2025-08-13 16:10:21 +0900

    style. 텍스트 추가
    요구사항 추가

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -4,6 +4,15 @@ import 'package:moviehomework/services/movieService.dart';
 import 'package:moviehomework/widgets/scroll_widget.dart';
 import 'package:moviehomework/widgets/text_widget.dart';
 
+// 이하 요구사항
+// (1). 앱은 2개 화면이 있어야 합니다: Home 그리고 Detail.
+// (2). Home 스크린은 아래와 같은 기능을 갖고있어야 합니다.
+// - 가장 인기 있는 영화 목록이 표시되어야 합니다.
+// - 극장에서 상영 중인 영화 목록이 표시되어야 합니다.
+// - 곧 개봉할 영화 목록이 표시되어야 합니다.
+// - movie를 탭하면 세부정보 화면으로 이동해야 합니다.
+// - 세부정보 화면에는 다음이 표시되어야 합니다.
+//    영화의 포스터/ 영화의 제목 / 영화의 등급 / 영화의 개요 / 영화의 장르
 class Home extends StatelessWidget {
   const Home({super.key});
 
