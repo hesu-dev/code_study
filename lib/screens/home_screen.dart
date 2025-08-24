8498613be7bf594e5de205e227ba120cdb2edfd1
sunell <hs0647@gmail.com>
2025-08-24 21:14:37 +0900

    feat

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,18 +10,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
+  final List<Map<String, int>> minutes = [
+    {"test": 2},
+    {"15": 900},
+    {"20": 1200},
+    {"25": 1500},
+    {"30": 1800},
+    {"35": 2100},
+  ];
+
+  int constMinutes = 0;
+  int totalSeconds = 0;
+  int totalRest = 0;
   bool isRuning = false;
   int totalPomodoros = 0;
+  int totalRound = 0;
   late Timer timer;
 
   void onTick(Timer timer) {
     if (totalSeconds == 0) {
       setState(() {
         totalPomodoros = totalPomodoros + 1;
+        totalRest = totalRest + 1;
         isRuning = false;
-        totalSeconds = twentyFiveMinutes;
+        totalSeconds = constMinutes;
       });
       timer.cancel();
     } else {
@@ -58,14 +70,11 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           Flexible(
-            flex: 3,
+            flex: 2,
             child: Container(
               width: 280,
               alignment: Alignment.center,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 4),
-              ),
+
               child: Text(
                 formet(totalSeconds),
                 style: TextStyle(
@@ -76,15 +85,72 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+          // Expanded(child:
+          // ),
           Flexible(
-            flex: 1,
+            flex: 0,
             child: Center(
-              child: ElevatedButton.icon(
-                onPressed: isRuning ? onPausePressed : onStartPressed,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: minutes.map((item) {
+                    final labelText = item.keys.first;
+                    final value = item.values.first;
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () => {
+                          setState(() {
+                            totalSeconds = value;
+                            constMinutes = value;
+                          }),
+                          if (totalSeconds != 0) {isRuning = false},
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 12,
+                          ),
+                          shape: StadiumBorder(),
+                        ),
+                        child: Text(
+                          '$labelText분',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Center(
+              child: IconButton(
+                onPressed: () {
+                  if (isRuning) {
+                    onPausePressed();
+                  } else {
+                    if (totalRest > 2) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('$totalPomodoros회가 지났으니 2초간 쉬어야 합니다.'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                      Future.delayed(Duration(seconds: 2));
+                      totalRest = 0;
+                      totalRound = totalRound + 1;
+                      return;
+                    }
+                    onStartPressed();
+                  }
+                },
                 icon: Icon(isRuning ? Icons.pause : Icons.play_arrow),
-                label: Text('집중 시작하기', style: TextStyle(fontSize: 20)),
                 style: ElevatedButton.styleFrom(
-                  // primary: Colors.white,
+                  iconColor: Colors.white,
                   // onPrimary: Colors.black,
                   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
                   shape: StadiumBorder(),
@@ -108,8 +174,12 @@ class _HomeScreenState extends State<HomeScreen> {
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            // IconButton(
+                            //   onPressed: () {},
+                            //   icon: Icon(Icons.book),
+                            // ),
                             Text(
-                              '기록',
+                              '$totalPomodoros/3',
                               style: TextStyle(
                                 fontSize: 20,
                                 color: Theme.of(
@@ -117,24 +187,44 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ).textTheme.headlineLarge!.color,
                               ),
                             ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.book),
+                            Text(
+                              'ROUND',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Theme.of(
+                                  context,
+                                ).textTheme.headlineLarge!.color,
+                              ),
                             ),
                           ],
                         ),
-                        // VerticalDivider(
-                        //   width: 100,
-                        //   thickness: 1.5,
-                        //   color: Colors.grey,
-                        //   indent: 10,
-                        //   endIndent: 10,
-                        // ),
+                        VerticalDivider(
+                          thickness: 1.5,
+                          color: Colors.grey,
+                          indent: 10,
+                          endIndent: 10,
+                        ),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            // IconButton(
+                            //   onPressed: () async {
+                            //     final url = Uri.parse(
+                            //       'https://www.youtube.com/watch?v=Hvi8UDXFikk',
+                            //     );
+                            //     if (await canLaunchUrl(url)) {
+                            //       await launchUrl(
+                            //         url,
+                            //         mode: LaunchMode.externalApplication,
+                            //       ); // 외부 앱에서 열기
+                            //     } else {
+                            //       throw 'Could not launch $url';
+                            //     }
+                            //   },
+                            //   icon: Icon(Icons.headset_outlined),
+                            // ),
                             Text(
-                              '브라운노이즈',
+                              '$totalRound/12',
                               style: TextStyle(
                                 fontSize: 20,
                                 color: Theme.of(
@@ -142,21 +232,14 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ).textTheme.headlineLarge!.color,
                               ),
                             ),
-                            IconButton(
-                              onPressed: () async {
-                                final url = Uri.parse(
-                                  'https://www.youtube.com/watch?v=Hvi8UDXFikk',
-                                );
-                                if (await canLaunchUrl(url)) {
-                                  await launchUrl(
-                                    url,
-                                    mode: LaunchMode.externalApplication,
-                                  ); // 외부 앱에서 열기
-                                } else {
-                                  throw 'Could not launch $url';
-                                }
-                              },
-                              icon: Icon(Icons.headset_outlined),
+                            Text(
+                              'GOAL',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Theme.of(
+                                  context,
+                                ).textTheme.headlineLarge!.color,
+                              ),
                             ),
                           ],
                         ),
