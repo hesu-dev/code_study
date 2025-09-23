c18daff45e2fad7ff1b1b57daee641316a99fc00
sunell <hs0647@gmail.com>
2025-09-23 15:57:55 +0900

    #1.1 Implicitly Animated Widgets

---
+++ animations_app/lib/screens/implicit_animations_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ImplicitAnimationsScreen extends StatefulWidget {
+  const ImplicitAnimationsScreen({super.key});
+
+  @override
+  State<ImplicitAnimationsScreen> createState() =>
+      _ImplicitAnimationsScreenState();
+}
+
+class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Implict Animations')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              duration: const Duration(seconds: 2),
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              child: AnimatedOpacity(
+                opacity: _visible ? 1 : 0.2,
+                duration: const Duration(seconds: 2),
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _trigger, child: const Text('Go!')),
+          ],
+        ),
+      ),
+    );
+  }
+}
